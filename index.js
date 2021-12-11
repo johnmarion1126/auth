27b8244450b590cc80af695d7334a366ef9796f0
John Marion Sanchez <johnmarion1126@gmail.com>
2021-12-11 11:31:28 -0800

    Add eslint to backend

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 const app = express();
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
 });
 
 app.listen(PORT, () => {
