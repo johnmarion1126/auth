5748a3340909bcdc14ca218bc5cb09f3ff3171df
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-14 13:09:25 -0800

    Add .env to backend

--- config/db.js
+++ config/db.js
@@ -5,11 +5,19 @@ import pg from "pg";
 const Pool = pg.Pool;
 
 const pool = new Pool({
-    user: "postgres", /*process.env.POSTGRES_USER,*/
-    password: "password", /*process.env.POSTGRES_PASSWORD,*/
-    host: "localhost", /*process.env.POSTGRES_HOST,*/
-    database: "ideas", /*process.env.POSTGRES_DATABASE*/
-    port: 5432 /*process.env.POSTGRES_PORT,*/
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
 });
+
+pool.connect(err => {
+  if (err) {
+    console.error('connection error', err.stack)
+  } else {
+    console.log('connected')
+  }
+})
 
 export default pool;
