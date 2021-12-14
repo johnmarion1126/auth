69232f85c9ed9986d0afd937da32eb5627c7cac8
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-14 13:20:19 -0800

    Add api call to get all users

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>');
 });
 
+app.get('/users', async (req, res) => {
+    try {
+        const allUsers = await pool.query("SELECT * FROM users");
+        res.json(allUsers.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Listen on the port ${PORT}...`);
 });
