f22914c1db5aa285a1f77c8dffe56d9d0056a071
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-15 13:23:21 -0800

    Fix eslint in backend

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,17 +1,17 @@
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' });
-}
+  res.status(404).send({ error: 'unknown endpoint' });
+};
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message);
+  console.error(err.message);
 
-    if (err.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).json({ error: err.message });
-    }
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  } if (err.name === 'ValidationError') {
+    return res.status(400).json({ error: err.message });
+  }
 
-    next(err);
-}
+  next(err);
+};
 
 export { unknownEndpoint, errorHandler };
