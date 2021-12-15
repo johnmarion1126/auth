9cb12e962058fe4ba42083f82c244b9e46859e33
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-15 13:23:21 -0800

    Fix eslint in backend

--- backend/index.js
+++ backend/index.js
@@ -3,40 +3,41 @@ import cors from 'cors';
 import pg from 'pg';
 
 import config from './utils/config.js';
-import { unknownEndpoint, errorHandler } from './utils/middleware.js';
 
 const app = express();
-const corsOptions = {origin: config.URL || '*'};
-const Pool = pg.Pool;
+const corsOptions = { origin: config.URL || '*' };
 
-const pool = new Pool({
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    host: config.DB_HOST,
-    database: config.DB_DATABASE,
-    port: config.DB_PORT,
+const pool = new pg.Pool({
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  host: config.DB_HOST,
+  database: config.DB_DATABASE,
+  port: config.DB_PORT,
 });
 
-pool.connect(err => {
+pool.connect((err) => {
   if (err) {
     console.error('error connecting to PostgreSQL', err.stack);
   } else {
-    console.log(`connected to PostgreSQL on ${config.PORT}`);
+    console.log(`connected to PostgreSQL on port ${config.PORT}`);
   }
-})
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+  res.send('<h1>Hello World!</h1>');
 });
 
 app.get('/users', async (req, res) => {
-    try {
-        const allUsers = await pool.query("SELECT * FROM users");
-        res.json(allUsers.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const allUsers = await pool.query('SELECT * FROM users');
+    res.json(allUsers.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
+
+// TODO: MOVE ROUTES INTO ROUTES FOLDER
+// TODO: CONNECT MIDDLEWARE
