55c4b43988e12c2fc56c28637d58e1a6d01b0fd4
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-22 18:12:39 -0800

    Create first test

---
+++ backend/tests/user.test.js
@@ -0,0 +1,16 @@
+import supertest from 'supertest';
+import pool from '../database/db.js';
+import app from '../app';
+
+const api = supertest(app);
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/users')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+}, 10000);
+
+afterAll(() => {
+  pool.end();
+});
