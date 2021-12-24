b0d0690d42d0a182948da53b6bee43e9e9f7ebdf
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-24 15:48:35 -0800

    Add test for the other api calls

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,12 +5,79 @@ import app from '../app';
 
 const api = supertest(app);
 
-test('notes are returned as json', async () => {
+describe('API Calls', () => {
+  test('a valid user can be added', async () => {
+    const newUser = {
+      userId: 1,
+      username: 'testusername',
+      password: 'testpassword',
+    };
+
+    const users = await api.get('/users');
+    const usersLength = users.body.length;
+
+    await api
+      .post('/users/create')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const newUsers = await api.get('/users');
+    const newUsersLength = newUsers.body.length;
+
+    expect(newUsersLength).toEqual(usersLength + 1);
+  });
+
+  test('users are returned as json', async () => {
+    await api
+      .get('/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  }, 10000);
+
+  test('a specific user can be viewed', async () => {
+    const resultUser = await api
+      .get('/users/1')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const resultUsername = resultUser.body[0].username;
+
+    expect(resultUsername).toEqual('testusername');
+  });
+
+  test(' a user can be updated', async () => {
+    const updatedUser = {
+      password: 'newpassword',
+    };
+
+    await api
+      .put('/users/update/1')
+      .send(updatedUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    const user = await api.get('/users/1');
+
+    const resultPassword = user.body[0].password;
+
+    expect(resultPassword).toEqual('newpassword');
+  });
+});
+
+test('a user can be deleted', async () => {
+  const users = await api.get('/users');
+  const usersLength = users.body.length;
+
   await api
-    .get('/users')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-}, 10000);
+    .delete('/users/delete/1')
+    .expect(200);
+
+  const newUsers = await api.get('/users');
+  const newUsersLength = newUsers.body.length;
+
+  expect(newUsersLength).toEqual(usersLength - 1);
+});
 
 afterAll(() => {
   pool.end();
