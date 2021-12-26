c2ee035f95946e8e37ab6aee81c72711f25224b5
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-25 20:45:36 -0800

    Refactor url routes

--- index.js
+++ index.js
@@ -10,7 +10,3 @@ const start = async () => {
 };
 
 start();
-
-// FIX TESTS
-// https://fullstackopen.com/en/part4/testing_the_backend
-// https://github.com/FullStack-HY/part3-notes-backend/tree/part4-4
