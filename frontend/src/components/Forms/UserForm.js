3c0b9bd67e5460a4d363142879be756eb7dc226d
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-26 23:24:33 -0800

    Create a simple form for name and password

--- frontend/src/components/Forms/UserForm.js
+++ frontend/src/components/Forms/UserForm.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const UserForm = () => (
-  <div>
-    UserForm
-  </div>
-);
+const UserForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  const [result, setResult] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setResult(`Username: ${username} Password: ${password}`);
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <form>
+      <div>Username</div>
+      <input
+        type="text"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <div>Password</div>
+      <input
+        type="text"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+      {result}
+    </form>
+  );
+};
 export default UserForm;
+
+// TODO: Finish user form
+// https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps
+// https://fullstackopen.com/en/part1/component_state_event_handlers#event-handling
