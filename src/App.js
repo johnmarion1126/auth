007094e20f21fb7076d2375a4250b2fc3f3363d0
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-14 20:49:22 -0800

    Move user form and create message component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import UserForm from './components/Forms/UserForm/UserForm';
+import UserForm from './components/UserForm/UserForm';
+import Message from './components/Message/Message';
 
 const App = () => (
   <div className="w-screen h-screen flex flex-col justify-center text-center items-center m-0 p-0 bg-slate-50">
     <UserForm />
+    <Message />
     <footer className="absolute p-1 px-4 bottom-0 w-screen bg-gray-900 text-indigo-500 text-sm flex justify-between">
       <a href="https://github.com/johnmarion1126/authentication" className="hover:opacity-70">
         johnmarion1126/messages
