1a87aac2162687cda38b5b7c8ba5e16b1caed17f
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-14 21:13:54 -0800

    Add styling for message form

--- frontend/src/components/Message/Message.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Message = () => (
-  <div>Message</div>
-);
-
-export default Message;
---
+++ frontend/src/components/MessageForm/Message.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MessageForm = () => (
+  <form>
+    <input className="w-80 md:w-96 m-10 p-4 border-gray-400 border-solid border-2 bg-white text-left focus:border-gray-900" placeholder="Write your message here" />
+  </form>
+);
+
+export default MessageForm;
