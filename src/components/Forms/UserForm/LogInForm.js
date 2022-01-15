fa83996f50a66e8764fa7ed03fc2624c37bd57e1
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-14 19:15:56 -0800

    Add styling for form

--- src/components/Forms/UserForm/LogInForm.js
+++ src/components/Forms/UserForm/LogInForm.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 const LogInForm = ({ username, password }) => (
   <div>
-    Log In
+    <h1 className="text-2xl font-bold">Log In</h1>
     <form>
-      <div className="underline">Username</div>
+      <div className="m-2 mt-4">Username</div>
       <input
         type={username.type}
         value={username.value}
         onChange={username.onChange}
-        className="border-solid border-black border-2"
+        className="border-solid border-gray-400 border-2 p-1 px-2"
       />
-      <div className="underline">Password</div>
+      <div className="m-2 mt-4">Password</div>
       <input
         type={password.type}
         value={password.value}
         onChange={password.onChange}
-        className="border-solid border-black border-2"
+        className="border-solid border-gray-400 border-2 p-1 px-2"
       />
     </form>
   </div>
