99ca00c76a9d3fd804e98d6a9e8b66ead02c93a2
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-15 14:58:00 -0800

    Add validation for sign up form

--- frontend/src/components/MessageForm/Message.js
+++ frontend/src/components/MessageForm/Message.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MessageForm = () => (
   <form>
-    <input className="w-80 md:w-96 m-10 p-4 border-gray-400 border-solid border-2 bg-white text-left focus:border-gray-900" placeholder="Write your message here" />
+    <input className="w-80 md:w-96 m-10 p-4 border-gray-400 border-solid border-2 bg-white text-left outline-indigo-500" placeholder="Write your message here" />
   </form>
 );
 
