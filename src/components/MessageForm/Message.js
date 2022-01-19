207522f57417a327059f136e0c0b3ce942e400c0
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-18 16:06:16 -0800

    Replace message with button

--- src/components/MessageForm/Message.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MessageForm = () => (
-  <form>
-    <input className="w-80 md:w-96 m-10 p-4 border-gray-400 border-solid border-2 bg-white text-left outline-indigo-500" placeholder="Write your message here" />
-  </form>
-);
-
-export default MessageForm;
