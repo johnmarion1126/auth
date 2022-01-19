da4de0ee74424b4e62782591f47ccf812d0f01fa
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-18 16:06:16 -0800

    Replace message with button

---
+++ frontend/src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthroziedButton = () => (
+  <button type="button" className="border-2 border-gray-400 px-6 py-2 rounded-md mt-6 text-white bg-gray-400">Press Me</button>
+);
+
+export default AuthroziedButton;
