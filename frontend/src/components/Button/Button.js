528851555fe4cddd338c16a453f86b6cb1ddc78f
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-19 22:03:15 -0800

    Add styling for button on login/signup

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const AuthroziedButton = () => (
-  <button type="button" className="border-2 border-gray-400 px-6 py-2 rounded-md mt-6 text-white bg-gray-400">Press Me</button>
-);
+import { useSelector } from 'react-redux';
+
+const AuthroziedButton = () => {
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
+  return (
+    <button type="button" className={!isLoggedIn ? 'border-2 border-gray-400 px-6 py-2 rounded-md mt-6 text-white bg-gray-400' : 'border-2 border-indigo-500 px-6 py-2 rounded-md mt-6 text-white bg-indigo-500'}>Press Me</button>
+
+  );
+};
 
 export default AuthroziedButton;
