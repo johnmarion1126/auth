8eef006742df8062bd7e72fc7aef6936f554b910
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2022-01-26 03:14:39 -0800

    Store token with cookies

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import config from './utils/config.js';
 import route from './routes/userRoutes.js';
 import { unknownEndpoint, errorHandler } from './utils/middleware.js';
 
 const app = express();
-const corsOptions = { origin: config.URL || '*' };
+const corsOptions = { credentials: true, origin: config.URL };
 
 app.use(helmet());
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 
 app.use(route);
